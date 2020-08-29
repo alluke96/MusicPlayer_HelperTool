98c35217e676453dac0f90021949a521d772b58b
Allyson Luan Dunke <57627628+alluke96@users.noreply.github.com>
2020-08-28 22:59:13 -0300

    added the C# script

---
+++ MusicPlayer.cs
@@ -0,0 +1,41 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+    private static MusicPlayer _instance;
+
+    [SerializeField] private SceneAsset[] scenesWhereTheObjectIsDestroyed;
+
+    private void Awake()
+    {
+        if (_instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        _instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        DestroyGameObject();
+    }
+
+    // Check if the current playing scene is one of the specified scenes to destroy the music player.
+    private void DestroyGameObject()
+    {
+        if (scenesWhereTheObjectIsDestroyed == null) return;
+        
+        foreach (var scene in scenesWhereTheObjectIsDestroyed)
+        {
+            if (SceneManager.GetActiveScene().name.Equals(scene.name))
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
